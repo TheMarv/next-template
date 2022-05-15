a2054f0d369256565233808e94848d3c8902b903
Marvin Steckel <themarvdoteu@gmail.com>
2022-05-15 11:02:10 +0200

    Add next-auth

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
 import { createGlobalStyle, ThemeProvider } from 'styled-components';
+import { SessionProvider } from 'next-auth/react';
 
 const GlobalStyles = createGlobalStyle`
   *,
@@ -18,14 +19,14 @@ const theme = {
   color: '#000',
 };
 
-function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps: { session, ...pageProps } }) {
   return (
-    <>
+    <SessionProvider session={session}>
       <GlobalStyles />
       <ThemeProvider theme={theme}>
         <Component {...pageProps} />
       </ThemeProvider>
-    </>
+    </SessionProvider>
   );
 }
 
