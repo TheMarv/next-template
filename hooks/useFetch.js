eb07a082e676111da0c06d11ed23ac3909ae3eea
Marvin Steckel <themarvdoteu@gmail.com>
2022-05-15 11:36:00 +0200

    Add useStore and useFetch

---
+++ hooks/useFetch.js
@@ -0,0 +1,25 @@
+import { useEffect, useState } from 'react';
+
+export default function useFetch(url) {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    setLoading(true);
+    setError(null);
+    if (url) {
+      fetch(url)
+        .then(response => response.json())
+        .then(json => {
+          setData(json);
+          setLoading(false);
+        })
+        .catch(error_ => {
+          setError(error_);
+          setLoading(false);
+        });
+    }
+  }, [url]);
+  return { data, loading, error };
+}
